7f1ca615387c81cc37e0df3f789a6edd6bd33309
kairungIT <113651426+kairungIT@users.noreply.github.com>
2024-03-29 10:56:01 +0700

    .

--- pages/3🎡Classification.py
+++ pages/3🎡Classification.py
@@ -63,9 +63,12 @@ import numpy as np
 if st.button("ทำนายผล"):
    # ทำนาย
    #dt = pd.read_csv("./data/breastcancer.csv") 
+   cols=['ClumpThickness', 'UniformityofCellSize',
+       'UniformityofCellShape', 'MarginalAdhesion', 'SingleEpithelialCellSize',
+       'BareNuclei', 'BlandChromatin', 'NormalNucleoli', 'Mitoses']
 
-   X = dt.iloc[:,1:11]
-   y = dt.Class   
+   X = dt[cols]
+   y = dt['Class   ']
 
    Knn_model = KNeighborsClassifier(n_neighbors=3)
    Knn_model.fit(X, y)
